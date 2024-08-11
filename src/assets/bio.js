ddb21c25e4b12b4a269871c086157d4639438f7a
riccardogiacomazzi <riccardogiacomazzi93@gmail.com>
2024-08-11 11:57:39 +0200

    analytics setup

--- src/assets/bio.js
+++ src/assets/bio.js
@@ -1,7 +1,7 @@
 const bio = {
-  text: "Tracya is a project that combines electronic music and visual arts. The distinctive blend of ambient, dub, and acid elements allows the artist to create imaginary soundscapes and layers, transporting the listener to a dystopian dimension where sound acts as a force that propels them into orbit. The mix of genres typical of his compositions ranges from downtempo to techno, for high-speed sets that magnetically capture the dancefloor. The deeply rooted glitch imprint of the visual sets perfectly blends with the sound, creating a pattern of abstract images and shapes in constant and unpredictable evolution.",
+  text: "Tracya is a project that combines electronic music and visual arts. The distinctive blend of ambient, dub, and acid elements allows me to create imaginary soundscapes and layers, transporting the listener to distant dimensions where sound acts as a force that propels them into orbit. The mix of genres typical of my compositions ranges from downtempo to techno. The deeply rooted glitch imprint of the visual sets blends with the sound, creating a pattern of abstract images and shapes in constant and unpredictable evolution.",
   visualArt:
-    "Tracya's visual art is deeply rooted in glitch art and takes inspiration from corrupted images and noisy textures. In order to achieve his aestethic, he uses a wide range of devices and tools such as: analog and digital cameras, old camcorders, DIY analog video generators, and TouchDesigner.",
+    "Tracya's aesthetic is deeply rooted in glitch art and takes inspiration from corrupted images and noisy textures. In order to achieve my aestethic, I use a wide range of devices and tools such as: analog and digital cameras, old camcorders, DIY analog video generators, and TouchDesigner.",
   pics: [
     {
       url: "https://live.staticflickr.com/65535/53908701183_e863008b26_o.jpg",
